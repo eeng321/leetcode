a25fe8b497179aea53d3a9c4e78c61dda7afb8bc
eeng <eeng@sfu.ca>
2020-09-18 21:49:50 -0300

    java balanced_binary_tree O(n)

---
+++ easy/balanced_binary_tree.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) == -1 ? false : true;
+    }
+    
+    public int height(TreeNode root) {
+        if(root == null) return 0;
+        
+        int l = height(root.left);
+        int r = height(root.right);
+        
+        if(Math.abs(l-r) > 1 || l == -1 || r == -1){
+            return -1;
+        }
+        return Math.max(l, r) + 1;
+    }
+}
