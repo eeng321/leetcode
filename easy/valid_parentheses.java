a518030c1d1497e1f1af436b2ae2170f353d3fed
eeng <eeng@sfu.ca>
2020-09-19 13:39:02 -0300

    java valid_parentheses O(n)

---
+++ easy/valid_parentheses.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() <= 1) return false;
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(isOpenBracket(c)){
+                stack.push(c);
+            } else {
+                if(!stack.empty() && isMatching(stack.peek(), c)){
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty() ? true : false;
+    }
+    
+    public boolean isOpenBracket(char c) {
+        return c == '{' || c == '(' || c == '[';   
+    }
+    
+    public boolean isMatching(char o, char c) {
+        if(o == '(' && c == ')') {
+            return true;
+        }
+        if(o == '{' && c == '}') {
+            return true;
+        }
+        if(o == '[' && c == ']') {
+            return true;
+        }
+        return false;
+    }
+}
