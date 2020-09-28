8bdec95bd03c5bb237839cfdedb0baf7647e6e17
eeng <eeng@sfu.ca>
2020-09-27 22:21:56 -0300

    java min_depth_BT iterative O(n)

---
+++ easy/min_depth_BT-iterative.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int d = 0;
+        while(!q.isEmpty()){
+            d++;
+            int size = q.size();
+            for(int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                if(curr == null) continue;
+                if(curr.left == null && curr.right == null) return d;
+                if(curr.left != null){
+                    q.add(curr.left);
+                }
+                if(curr.right != null){
+                    q.add(curr.right);
+                }
+            }
+        }
+        return d;
+    }
+}
