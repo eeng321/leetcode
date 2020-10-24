3405976f821e16c2ffa1b0a5ceda1021570c8522
eeng <eeng@sfu.ca>
2020-10-24 15:38:38 -0300

    java delete_node_BST O(h)

---
+++ medium/delete_node_BST.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            // found key
+            if (root.left == null && root.right == null) {
+                return null;
+            } else if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            } else {
+                // both children exist
+                int min = findMin(root.right);
+                root.val = min;
+                root.right = deleteNode(root.right, min);
+            }
+        }
+        return root;
+    }
+    
+    private int findMin(TreeNode curr) {
+        if (curr.left == null) {
+            return curr.val;
+        }
+        return findMin(curr.left);
+    }
+}
